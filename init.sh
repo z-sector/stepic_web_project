41ef20e5fdd75290624ac2c688a141b1bbcfd248
George Ramaniuk <george.ramaniuk@gmail.com>
2018-11-03 17:26:16 +0300

    Add gunicorn

--- init.sh
+++ init.sh
@@ -2,8 +2,8 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+# sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
 sudo /etc/init.d/gunicorn restart
 
-sudo /etc/init.d/mysql start
-mysql -uroot -e "create database qa"
+# sudo /etc/init.d/mysql start
+# mysql -uroot -e "create database qa"
