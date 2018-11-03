806e5ff443250c6f9a79779edad9da9f33a7c226
George Ramaniuk <george.ramaniuk@gmail.com>
2018-11-03 13:51:51 +0300

    Add gunicorn

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
-#!/usr/bin/python
 def app(environ, start_response):
-    data = ''
-    for line in environ["QUERY_STRING"].split("&"):
-        data = data+line+"\n"
-    start_response('200 OK', [('Content-Type', 'text/plain')])
+    """Simplest possible application object"""
+    data = '\n'.join(environ['QUERY_STRING'].split('&'))
+    status = '200 OK'
+    response_headers = [
+        ('Content-type','text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
     return [data]
