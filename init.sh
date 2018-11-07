03cb70d69c67c0f0dc6729dd9b9cd2d03c348f0d
George Ramaniuk <george.ramaniuk@gmail.com>
2018-11-07 16:05:41 +0300

    First project

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@ sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
-sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+sudo ln -sf /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/django.example
 sudo /etc/init.d/gunicorn restart
 
 # sudo /etc/init.d/mysql start
